245f0db523b8c8e60bb4129a4b44bb9a43a5e8c6
Ronak Garg <ronak.garg@coviam.com>
2018-07-12 11:35:11 +0530

    minor changes

--- Todo/controller/CategoryTableViewController.swift
+++ Todo/controller/CategoryTableViewController.swift
@@ -64,7 +64,6 @@ class CategoryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         performSegue(withIdentifier: "goToItems", sender: self)   //go to item by clickinh category
-        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
